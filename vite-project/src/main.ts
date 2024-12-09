7cf9f44a58b0592e07f5d47553da3cb1c499391c
Ymer-MI <ymer.nordstrom@medieinstitutet.se>
2024-12-09 12:36:32 +0100

    Updated basic styling and added element creation helpers

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,1 +1,4 @@
 import './style.css'
+import { e as elements } from "./helpers/elementCreationHelpers";
+
+document.getElementById("app")?.append(elements.heading.primary(`Test`), elements.heading.secondary(`Testing that the element helpers are working`));
