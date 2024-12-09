e6ded31c9353871d36ebdaec5cd72b0c552110d6
Ymer-MI <ymer.nordstrom@medieinstitutet.se>
2024-12-09 14:00:49 +0100

    Added more elements to the helper

--- vite-project/src/helpers/elementCreationHelpers.ts
+++ vite-project/src/helpers/elementCreationHelpers.ts
@@ -7,6 +7,12 @@ export const e = (() => {
             })
         }, createHeading = (n: number, t: string, cN?: string, id?:string) => {
             return createElement(`h${n}`, t, `heading ${cN || ''}`.trim(), id) as HTMLHeadingElement
+        }, createInpOrBtn = (e: string, str?: string, cN?: string, t?: string, id?: string) => {
+            const el = createElement(e, cN, id);
+            return Object.assign(e === 'input' ? el as HTMLInputElement : el as HTMLButtonElement, {
+                innerHTML: str,
+                type: t || ''
+            })
         }
 
         return {
@@ -21,19 +27,17 @@ export const e = (() => {
                     return createHeading(3, text, className, id);
                 }
             },
-            div: (className: string, id?: string) => {
+            div: (className?: string, id?: string) => {
                 return createElement('div', className, id) as HTMLDivElement;
             },
-            span: (className: string, id?: string) => {
+            span: (className?: string, id?: string) => {
                 return createElement('span', className, id) as HTMLSpanElement;
             },
-            input: (type: string, className: string, id?: string) => {
-                return Object.assign(createElement('input', className, id) as HTMLInputElement, {
-                    type: type
-                });
+            input: (type?: string, className?: string, id?: string) => {
+                return createInpOrBtn('input', className, type || 'text', id) as HTMLInputElement;
             },
-            button: (className: string, id?: string) => {
-                return createElement('button', className, id) as HTMLButtonElement;
+            button: (text: string, type?: string, className?: string, id?: string) => {
+                return createInpOrBtn('button', text, className, type, id) as HTMLButtonElement;
             }
         }
 })();
