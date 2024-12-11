4229d5845cf564df44bf7bdc78a5599a0be3fce6
Ymer-MI <ymer.nordstrom@medieinstitutet.se>
2024-12-11 11:30:40 +0100

    Updated settings and exampels to showcase how to use it.

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -2,12 +2,20 @@ import './style.css';
 import { e as elements } from './helpers/elementCreationHelpers';
 import { Task } from './models/Task';
 
+const DEFAULT_HEADING_CLASSES = elements.settings.defaultHeadingClass.set('heading');
+
+console.log(
+    {DEFAULT_HEADING_CLASSES: DEFAULT_HEADING_CLASSES},
+    {elements_settings_defaultHeadingClass_get: elements.settings.defaultHeadingClass.get()}
+);
+
 document.getElementById("app")?.append(
   elements.heading.primary(`Test`, 'primary'), 
   elements.heading.secondary(`Testing that the element helpers are working`),
   elements.input('text', 'Placeholder'),
-  elements.input('email', 'ymer.nordstrom@medieinstitutet.se', true),
+  elements.input('email', 'some.email@mail.com', true),
   elements.input('checkbox', true, true),
+  elements.input('checkbox'),
   elements.button('Button')
 );
 
