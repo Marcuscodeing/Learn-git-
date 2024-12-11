c2570fc2db254666a6bd90659a6c8ada7306f479
Marcus Linder <marcuslinder@Marcuss-MacBook-Air.local>
2024-12-11 14:38:10 +0100

    skapat Class, constructor, lista

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -7,3 +7,22 @@ document.getElementById("app")?.append(
   elements.input(),
   elements.button('Button')
 );
+
+
+class Entodo {
+  name: string;
+  isComplete: boolean;
+
+constructor (name: string, isComplete: boolean = false){
+this.name = name;
+this.isComplete = isComplete;
+
+}}
+
+const study = new Entodo ("Studera")
+const workout = new Entodo ("Träna")
+const clean = new Entodo ("Städa")
+const meditate = new Entodo ("Meditera")
+const food = new Entodo ("Handla mat")
+
+let list: Entodo [] = [study, workout, clean, meditate, food];
