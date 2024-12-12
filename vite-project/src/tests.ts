6344ed8b99b3bf5081808d2e420428de7e257f70
Ymer-MI <ymer.nordstrom@medieinstitutet.se>
2024-12-12 11:35:31 +0100

    Refactored tests out to it's own file

---
+++ vite-project/src/tests.ts
@@ -0,0 +1,31 @@
+import { e as elements } from './helpers/elementCreationHelpers';
+import { Task } from './models/Task';
+
+export const t = () => {
+  const DEFAULT_HEADING_CLASSES = elements.settings.defaultHeadingClass.set('heading'), formID = 'testForm';
+
+  console.log(
+    {DEFAULT_HEADING_CLASSES: DEFAULT_HEADING_CLASSES},
+    {elements_settings_defaultHeadingClass_get: elements.settings.defaultHeadingClass.get()}
+  );
+
+  document.getElementById("app")?.append(
+    elements.heading.primary(`Test`, 'primary'),
+    elements.heading.secondary(`Testing that the element helpers are working`),
+    elements.input('text', 'Placeholder'),
+    elements.input('email', 'some.email@mail.com', true),
+    elements.input('checkbox', true),
+    elements.input('checkbox'),
+    elements.button('Button'),
+    elements.form([elements.input(), elements.input('email', 'some.email@mail.com'), elements.input('checkbox')], 'form', formID)
+  );
+
+  document.getElementById(formID)?.append(elements.button('Secound Button'));
+
+  const lSName = 'tasks', lS = localStorage.getItem(lSName),
+  tasks: Task[] = lS ? JSON.parse(lS).map((t: Task) => t as Task) : [new Task('hello world')];
+
+  console.log(tasks);
+
+  localStorage.setItem(lSName, JSON.stringify(tasks));
+};
