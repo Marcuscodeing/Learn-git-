b6843a496f543c8b10ed453487aad3c33fc57174
Ymer-MI <ymer.nordstrom@medieinstitutet.se>
2024-12-12 11:28:16 +0100

    Added some elements and updated tests.

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -2,7 +2,7 @@ import './style.css';
 import { e as elements } from './helpers/elementCreationHelpers';
 import { Task } from './models/Task';
 
-const DEFAULT_HEADING_CLASSES = elements.settings.defaultHeadingClass.set('heading');
+const DEFAULT_HEADING_CLASSES = elements.settings.defaultHeadingClass.set('heading'), formID = 'testForm';
 
 console.log(
     {DEFAULT_HEADING_CLASSES: DEFAULT_HEADING_CLASSES},
@@ -10,14 +10,17 @@ console.log(
 );
 
 document.getElementById("app")?.append(
-  elements.heading.primary(`Test`, 'primary'), 
-  elements.heading.secondary(`Testing that the element helpers are working`),
-  elements.input('text', 'Placeholder'),
-  elements.input('email', 'some.email@mail.com', true),
-  elements.input('checkbox', true, true),
-  elements.input('checkbox'),
-  elements.button('Button')
+    elements.heading.primary(`Test`, 'primary'),
+    elements.heading.secondary(`Testing that the element helpers are working`),
+    elements.input('text', 'Placeholder'),
+    elements.input('email', 'some.email@mail.com', true),
+    elements.input('checkbox', true),
+    elements.input('checkbox'),
+    elements.button('Button'),
+    elements.form([elements.input(), elements.input('email', 'some.email@mail.com'), elements.input('checkbox')], 'form', formID)
 );
+
+document.getElementById(formID)?.append(elements.button('Secound Button'));
 
 const lSName = 'tasks', lS = localStorage.getItem(lSName),
   tasks: Task[] = lS ? JSON.parse(lS).map((t: Task) => t as Task) : [new Task('hello world')];
